772b7deb7e85bcaea1931cbbb6e2f996cb819011
nguye374 <nguye374@sheridancollege.ca>
2021-06-16 20:56:08 -0400

    3rd commit for CSS

--- assignment2/js/scripts.js
+++ assignment2/js/scripts.js
@@ -30,17 +30,17 @@ date = date.toDateString();
             
             function parseJSON(data) {
             $.each(data,(key,val) => {
-                    
                     key = val.report_date;
                     localStorage.setItem(key,JSON.stringify(val));
                 })
             }
 
-            $("#result").html("Data from JSON file saved to localStorage!")
+            $("#result").html("Success!")
         });
 
         const array = [];
         $("#display").click(() => {
+            $("#title").html("Summary of Total Vaccine Doses Administered in Ontario");
             for (i=0;i<localStorage.length;i++){
                 array[i] = localStorage.key(i);
             }
@@ -52,7 +52,16 @@ date = date.toDateString();
             
         });
 
-        $("ul").on('click','li .info',(key) => {
-            $("#detailinfo").html(localStorage.getItem(key));
+        $("ul").on("click", "li", function(key) {
+            key = $(this).text();
+            var string = localStorage.getItem(key);
+            var vaccine = new Vaccine();
+            vaccine = JSON.parse(string);
+            string = "Report date: " + vaccine.report_date +
+            "<br>Previous day doses administered: " + vaccine.previous_day_doses_administered +
+            "<br>Total doses administered: " + vaccine.total_doses_administered +
+            "<br>Total doses in fully vaccinated individuals: " + vaccine.total_doses_in_fully_vaccinated_individuals +
+            "<br>Total individuals fully vaccinated: " + vaccine.total_individuals_fully_vaccinated;
+            $("#detailinfo").html(string);
         })
     });
